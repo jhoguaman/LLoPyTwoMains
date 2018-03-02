227fac5d06a7f4a7f8645218947bdae9571ca320
jhoguaman <jho.guaman@gmail.com>
2018-03-02 18:09:35 -0500

    Initial commit

---
+++ lib/multimain.py
@@ -0,0 +1,11 @@
+
+
+f = open('fiiles','r')
+main=f.readline()
+f.close()
+
+class MiClase:
+    """Simple clase de ejemplo"""
+    i = 12345
+    def f(self):
+        return 'hola mundo'
---
+++ main.py
@@ -0,0 +1,13 @@
+import pycom
+import time
+from multimain import MiClase
+
+
+pycom.heartbeat(False)
+for cycles in range(0): # stop after 10 cycles
+    pycom.rgbled(0x007f00) # green
+    time.sleep(2)
+    pycom.rgbled(0x7f7f00) # yellow
+    time.sleep(1.5)
+    pycom.rgbled(0x7f0000) # red
+    time.sleep(1)
